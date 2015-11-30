74fd9ec970c9608ee2f34e46e746800b68076ca5
zsl <zu459632879@126.com>
2015-11-30 19:30:07 +0800

    upload hw

---
+++ hw1130/121.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        if(prices.length<2)return 0;
+        int min=prices[0];
+        
+        for(int i=1;i<prices.length;i++)
+        {
+           if(prices[i]<min)min=prices[i];
+           if(prices[i]-min>=max)max=prices[i]-min;
+        }
+        return max;
+    }
+}
